c744e158c879221b088168d745282e2a7d74a9fe
肖雄 <apple@xiaoxiongdeMacBook-Air.local>
2017-04-24 08:36:41 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,6 +10,7 @@ class GroupsController < ApplicationController
 
  def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
    end
 
    def edit
@@ -40,7 +41,7 @@ def create
  end
 
    def destroy
-  
+
     @group.destroy
 
     redirect_to groups_path, alert: "Group deleted"
